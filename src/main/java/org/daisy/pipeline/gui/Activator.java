6d8b5c85447b37c4c6137522e923e9555b6c5789
capitancambio <capitan.cambio@gmail.com>
2015-03-27 12:52:11 +0000

    Modify activator to have a fx scene working

--- src/main/java/org/daisy/pipeline/gui/Activator.java
+++ src/main/java/org/daisy/pipeline/gui/Activator.java
@@ -16,187 +16,203 @@ import org.slf4j.LoggerFactory;
 
 import com.google.common.util.concurrent.Monitor;
 
-/**
- * OSGI activator for the GUI
- */
-public class Activator implements BundleActivator, Runnable {
+import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import javafx.application.Application;
 
-	private MainWindow window = null;
-	private ScriptRegistry scriptRegistry = null;
-	private JobManagerFactory jobManagerFactory = null;
-	private EventBusProvider eventBusProvider = null;
-	private WebserviceStorage webserviceStorage = null;
-	private BundleContext bundleContext = null;
+public class Activator extends Application implements BundleActivator,Runnable
+{
+        Stage stage;
+        
+        private ScriptRegistry scriptRegistry = null;
+        private JobManagerFactory jobManagerFactory = null;
+        private EventBusProvider eventBusProvider = null;
+        private WebserviceStorage webserviceStorage = null;
+        private BundleContext bundleContext = null;
+        private static final Logger logger = LoggerFactory.getLogger(Activator.class);
+        private final Monitor monitor = new Monitor();
+        private final Monitor.Guard pipelineServicesAvailable = new Monitor.Guard(monitor) {
+                public boolean isSatisfied() {
+                        return scriptRegistry != null && jobManagerFactory != null
+                                && webserviceStorage != null && eventBusProvider != null;
+                }
+        };
+        public void start(final Stage stage) {
+                Platform.runLater(new Runnable()
+                                {
+                                        @Override
+                                        public void run()
+                {
+                        Activator.this.stage = stage;
+                        BorderPane pane = new BorderPane();
+                        Scene scene = new Scene(pane, 400, 200);
+                        pane.setCenter(new Label("This is a JavaFX Scene in a Stage"));
+                        stage.setScene(scene);
+                        stage.show();
+                }
+                });
+        }
+        @Override
+        public void run() 
+        {
+                //Otherwise launch will block
+                new Thread(){
+                        public void run(){
+                                javafx.application.Application.launch(Activator.class);
+                        }
+                }.start();
+                System.out.println("Main Module is loaded!");
+        }
+        public void start(final BundleContext context) throws Exception {
+                bundleContext = context;
+                context.registerService(Runnable.class.getName(), this, null);
 
-	private static final Logger logger = LoggerFactory.getLogger(Activator.class);
-	
-	private final Monitor monitor = new Monitor();
-	private final Monitor.Guard pipelineServicesAvailable = new Monitor.Guard(monitor) {
-		public boolean isSatisfied() {
-			return scriptRegistry != null && jobManagerFactory != null 
-					&& webserviceStorage != null && eventBusProvider != null;
-		}
-	};
-	
-	
-	public void run() {
-		try {
-			monitor.enterWhen(pipelineServicesAvailable);
-			logger.debug("Running GUI");
-			window = new MainWindow(null, scriptRegistry, jobManagerFactory, 
-					webserviceStorage.getClientStorage().defaultClient(), eventBusProvider,
-					bundleContext);
-			window.setBlockOnOpen(true);
-		    window.open();
-		    Display.getCurrent().dispose();
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} 
-		finally {
-			monitor.leave();
-		}
-		
-	}
+                ServiceTracker<ScriptRegistry, ScriptRegistry> scriptRegistryTracker = new ServiceTracker<ScriptRegistry, ScriptRegistry>(
+                                context, ScriptRegistry.class,
+                                new ServiceTrackerCustomizer<ScriptRegistry, ScriptRegistry>() {
 
-	
+                                        @Override
+                                        public ScriptRegistry addingService(ServiceReference<ScriptRegistry> reference) {
+                                                logger.debug("Setting script registry");
+                                                monitor.enter();
+                                                try {
+                                                        scriptRegistry = context.getService(reference);
+                                                } 
+                                                finally {
+                                                        monitor.leave();
+                                                }
 
+                                                return scriptRegistry;
+                                        }
 
-	public void start(final BundleContext context) throws Exception {
-		bundleContext = context;
-		context.registerService(Runnable.class.getName(), this, null);
-		
-		ServiceTracker<ScriptRegistry, ScriptRegistry> scriptRegistryTracker = new ServiceTracker<ScriptRegistry, ScriptRegistry>(
-				context, ScriptRegistry.class,
-				new ServiceTrackerCustomizer<ScriptRegistry, ScriptRegistry>() {
+                @Override
+                public void modifiedService(ServiceReference<ScriptRegistry> reference, ScriptRegistry service) {
+                        // TODO replace script registry ?
 
-					@Override
-					public ScriptRegistry addingService(ServiceReference<ScriptRegistry> reference) {
-						logger.debug("Setting script registry");
-						monitor.enter();
-						try {
-							scriptRegistry = context.getService(reference);
-						} 
-						finally {
-							monitor.leave();
-						}
-						
-						return scriptRegistry;
-					}
+                }
 
-					@Override
-					public void modifiedService(ServiceReference<ScriptRegistry> reference, ScriptRegistry service) {
-						// TODO replace script registry ?
+                @Override
+                public void removedService(ServiceReference<ScriptRegistry> reference, ScriptRegistry service) {
+                        // TODO close window ?
+                }
+                });
+                scriptRegistryTracker.open();
 
-					}
+                ServiceTracker<JobManagerFactory, JobManagerFactory> jobManagerFactoryTracker = new ServiceTracker<JobManagerFactory, JobManagerFactory>(
+                                context,
+                                JobManagerFactory.class,
+                                new ServiceTrackerCustomizer<JobManagerFactory, JobManagerFactory>() {
 
-					@Override
-					public void removedService(ServiceReference<ScriptRegistry> reference, ScriptRegistry service) {
-						// TODO close window ?
-					}
-				});
-		scriptRegistryTracker.open();
-		
-		ServiceTracker<JobManagerFactory, JobManagerFactory> jobManagerFactoryTracker = new ServiceTracker<JobManagerFactory, JobManagerFactory>(
-				context,
-				JobManagerFactory.class,
-				new ServiceTrackerCustomizer<JobManagerFactory, JobManagerFactory>() {
+                                        @Override
+                                        public JobManagerFactory addingService(ServiceReference<JobManagerFactory> reference) {
+                                                logger.debug("Setting job manager factory");
+                                                monitor.enter();
+                                                try {
+                                                        jobManagerFactory  = context.getService(reference);
+                                                } 
+                                                finally {
+                                                        monitor.leave();
+                                                }
 
-					@Override
-					public JobManagerFactory addingService(ServiceReference<JobManagerFactory> reference) {
-						logger.debug("Setting job manager factory");
-						monitor.enter();
-						try {
-							jobManagerFactory  = context.getService(reference);
-						} 
-						finally {
-							monitor.leave();
-						}
-						
-						return jobManagerFactory;
-					}
+                                                return jobManagerFactory;
+                                        }
 
-					@Override
-					public void modifiedService(ServiceReference<JobManagerFactory> reference, JobManagerFactory service) {
-						// TODO replace job manager ?
+                @Override
+                public void modifiedService(ServiceReference<JobManagerFactory> reference, JobManagerFactory service) {
+                        // TODO replace job manager ?
 
-					}
+                }
 
-					@Override
-					public void removedService(ServiceReference<JobManagerFactory> reference, JobManagerFactory service) {
-						// TODO close window ?
-					}
-				});
-		jobManagerFactoryTracker.open();
-		
-		ServiceTracker<WebserviceStorage, WebserviceStorage> webserviceStorageTracker = new ServiceTracker<WebserviceStorage, WebserviceStorage>(
-				context,
-				WebserviceStorage.class,
-				new ServiceTrackerCustomizer<WebserviceStorage, WebserviceStorage>() {
+                @Override
+                public void removedService(ServiceReference<JobManagerFactory> reference, JobManagerFactory service) {
+                        // TODO close window ?
+                }
+                });
+                jobManagerFactoryTracker.open();
 
-					@Override
-					public WebserviceStorage addingService(ServiceReference<WebserviceStorage> reference) {
-						logger.debug("Setting web service storage");
-						monitor.enter();
-						try {
-							webserviceStorage  = context.getService(reference);
-						} 
-						finally {
-							monitor.leave();
-						}
-						
-						return webserviceStorage;
-					}
+                ServiceTracker<WebserviceStorage, WebserviceStorage> webserviceStorageTracker = new ServiceTracker<WebserviceStorage, WebserviceStorage>(
+                                context,
+                                WebserviceStorage.class,
+                                new ServiceTrackerCustomizer<WebserviceStorage, WebserviceStorage>() {
 
-					@Override
-					public void modifiedService(ServiceReference<WebserviceStorage> reference, WebserviceStorage service) {
-						// TODO replace webservice storage?
+                                        @Override
+                                        public WebserviceStorage addingService(ServiceReference<WebserviceStorage> reference) {
+                                                logger.debug("Setting web service storage");
+                                                monitor.enter();
+                                                try {
+                                                        webserviceStorage  = context.getService(reference);
+                                                } 
+                                                finally {
+                                                        monitor.leave();
+                                                }
 
-					}
+                                                return webserviceStorage;
+                                        }
 
-					@Override
-					public void removedService(ServiceReference<WebserviceStorage> reference, WebserviceStorage service) {
-						// TODO close window ?
-					}
-				});
-		webserviceStorageTracker.open();
+                @Override
+                public void modifiedService(ServiceReference<WebserviceStorage> reference, WebserviceStorage service) {
+                        // TODO replace webservice storage?
 
-		ServiceTracker<EventBusProvider, EventBusProvider> eventBusProviderTracker = new ServiceTracker<EventBusProvider, EventBusProvider>(
-				context,
-				EventBusProvider.class,
-				new ServiceTrackerCustomizer<EventBusProvider, EventBusProvider>() {
+                }
 
-					@Override
-					public EventBusProvider addingService(ServiceReference<EventBusProvider> reference) {
-						logger.debug("Setting web service storage");
-						monitor.enter();
-						try {
-							eventBusProvider  = context.getService(reference);
-						} 
-						finally {
-							monitor.leave();
-						}
-						
-						return eventBusProvider;
-					}
+                @Override
+                public void removedService(ServiceReference<WebserviceStorage> reference, WebserviceStorage service) {
+                        // TODO close window ?
+                }
+                });
+                webserviceStorageTracker.open();
 
-					@Override
-					public void modifiedService(ServiceReference<EventBusProvider> reference, EventBusProvider service) {
-						// TODO replace event bus provider?
+                ServiceTracker<EventBusProvider, EventBusProvider> eventBusProviderTracker = new ServiceTracker<EventBusProvider, EventBusProvider>(
+                                context,
+                                EventBusProvider.class,
+                                new ServiceTrackerCustomizer<EventBusProvider, EventBusProvider>() {
 
-					}
+                                        @Override
+                                        public EventBusProvider addingService(ServiceReference<EventBusProvider> reference) {
+                                                logger.debug("Setting web service storage");
+                                                monitor.enter();
+                                                try {
+                                                        eventBusProvider  = context.getService(reference);
+                                                } 
+                                                finally {
+                                                        monitor.leave();
+                                                }
 
-					@Override
-					public void removedService(ServiceReference<EventBusProvider> reference, EventBusProvider service) {
-						// TODO close window ?
-					}
-				});
-		eventBusProviderTracker.open();
-		
-	}
+                                                return eventBusProvider;
+                                        }
 
-	public void stop(BundleContext context) throws Exception {
-		this.window.exit();
-	}
+                @Override
+                public void modifiedService(ServiceReference<EventBusProvider> reference, EventBusProvider service) {
+                        // TODO replace event bus provider?
+
+                }
+
+                @Override
+                public void removedService(ServiceReference<EventBusProvider> reference, EventBusProvider service) {
+                        // TODO close window ?
+                }
+                });
+                eventBusProviderTracker.open();
+
+        }
+        @Override
+        public void stop(BundleContext context) throws Exception
+        {
+                Platform.runLater(new Runnable()
+                                {
+                                        @Override
+                                        public void run()
+                {
+                        stage.close();
+                }
+                });
+                System.out.println("Main Module is unloaded!");
+        }
+
 
 }
